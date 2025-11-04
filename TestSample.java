e48dff24f5e0b2bde2692e8215bbf0077510b997
Uday Naik <udaynaik057@gmail.com>
2025-11-04 17:06:47 +0530

    Create TestSample.java

---
+++ TestSample.java
@@ -0,0 +1,14 @@
+package com.uday.website;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestSample {
+
+    @Test
+    public void testAddition() {
+        int expected = 5;
+        int actual = 2 + 3;
+        assertEquals(expected, actual);
+    }
+}
