a8ca53a66bcc030d6b9353f370648292ca5746a5
Uday Naik <udaynaik057@gmail.com>
2025-11-04 17:35:55 +0530

    Add unit test for addition functionality

---
+++ src/test/java/com/uday/website/TestSample.java
@@ -0,0 +1,12 @@
+package com.uday.website;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestSample {
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
